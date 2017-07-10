3771b16ac7ff52ce9f50112e8728915fa487ffd0
cats <catskittens@users.noreply.github.com>
2017-07-10 22:30:09 +0300

    Update terminal.js

--- terminal.js
+++ terminal.js
@@ -9,79 +9,84 @@ $(document).ready(function() {
     "white": {"text": "#000000", "bg": "#FFFFFF"},
     "matrix": {"text": "#0FFF00", "bg": "#000000"}
   };
+  
+  function printReply(text) {
+    $(".input").after("<br>"+text);
+  }
+  
   function parseCmd(cmd) {
     if (cmd.split(" ")[0] === "sudo") {
 
       if (cmd.split(" ")[1] !== undefined) {
         if (cmd.split(" ")[1] === "make") {
           if (cmd.split(" ")[2] === "me" && cmd.split(" ")[3] === "a" && cmd.split(" ")[4] === "sandwich") {
-            $(".input").after("<br>Okay.");
+            printReply("Okay.");
           } else if (cmd.split(" ")[2] === "me" && cmd.split(" ")[3] === "a" && cmd.split(" ")[4] === "cake") {
-            $(".input").after("<br>The cake is still a lie.");
+            printReply("The cake is still a lie.");
           } else {
-            $(".input").after("<br>sudo: make it yourself");
+            printReply("sudo: make it yourself");
           }
         }
 
         else if (cmd.split(" ")[1] === "ls" || cmd.split(" ")[1] === "cd") {
-          $(".input").after("<br>Listen, sudo is not going to help you here. There is still no files.")
+          printReply("Listen, sudo is not going to help you here. There is still no files.")
         }
 
         else if (cmd.split(" ")[1] === "help") {
-          $(".input").after("<br>List of useful commands:<br>make&nbsp;&nbsp;&nbsp;&nbsp;Makes something, I guess.")
+          printReply("List of useful commands:<br>make&nbsp;&nbsp;&nbsp;&nbsp;Makes something, I guess.")
         }
 
         else {
-          $(".input").after("<br>sudo: "+cmd.split(" ")[1]+": command not found");
+          printReply("sudo: "+cmd.split(" ")[1]+": command not found");
         }
       }
 
       else {
-        $(".input").after("<br>sudo: no command specified");
+        printReply("sudo: no command specified");
       }
     } else {
 
       if (cmd.split(" ")[0] === "make") {
         if (cmd.split(" ")[1] === "me" && cmd.split(" ")[2] === "a" && cmd.split(" ")[3] === "sandwich") {
-          $(".input").after("<br>What? Make it yourself.");
+          printReply("What? Make it yourself.");
         } else if (cmd.split(" ")[1] === "me" && cmd.split(" ")[2] === "a" && cmd.split(" ")[3] === "cake") {
-          $(".input").after("<br>The cake is a lie.");
+          printReply("The cake is a lie.");
         } else {
-          $(".input").after("<br>make: make it yourself");
+          printReply("make: make it yourself");
         }
       }
 
       else if (cmd.split(" ")[0] === "help") {
-        $(".input").after("<br>List of useful commands:<br>load&nbsp;&nbsp;&nbsp;&nbsp;Loads something. Type \"load --help\" for help on loading.<br>make&nbsp;&nbsp;&nbsp;&nbsp;Makes something, I guess.<br>sudo&nbsp;&nbsp;&nbsp;&nbsp;Administrator privileges (for everyone). Type \"sudo help\" for help with sudo commands.<br>setbg&nbsp;&nbsp;&nbsp;Followed by a hex code. Sets the terminal background colour.<br>settext Followed by a hex code. Sets the terminal text colour.<br>theme&nbsp;&nbsp;&nbsp;Followed by a theme name. Sets the terminal colours according to chosen theme. Type \"theme --help\" for a list of themes.");
+        printReply("List of useful commands:<br>load&nbsp;&nbsp;&nbsp;&nbsp;Loads something. Type \"load --help\" for help on loading.<br>make&nbsp;&nbsp;&nbsp;&nbsp;Makes something, I guess.<br>sudo&nbsp;&nbsp;&nbsp;&nbsp;Administrator privileges (for everyone). Type \"sudo help\" for help with sudo commands.<br>setbg&nbsp;&nbsp;&nbsp;Followed by a hex code. Sets the terminal background colour.<br>settext Followed by a hex code. Sets the terminal text colour.<br>theme&nbsp;&nbsp;&nbsp;Followed by a theme name. Sets the terminal colours according to chosen theme. Type \"theme --help\" for a list of themes.");
       }
 
       else if (cmd.split(" ")[0] === "load") {
         var loadThis = cmd.split(" ")[1];
-        if (loadThis === "--help") {
-          $(".input").after("<br>cats&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Loads kittens. :3<br>about&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Loads some stuff about me.<br>languages Loads info about my knowledge on programming languages.");
+        if (loadThis === "--help" || loadThis === "-h") {
+          printReply("cats&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Loads kittens. :3<br>about&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Loads some stuff about me.<br>languages Loads info about my knowledge on programming languages.");
         } else if (loadThis === "cats") {
-          $(".input").after("<br>Loading cats...");
+          printReply("Loading cats...");
           setTimeout(function() {
             window.location.href = "https://catskittens.github.io/cats.html";
           }, 500)
         } else if (loadThis === "about") {
-          $(".input").after("<br>Hi. I'm a cat. I live in Estonia. Meow.");
+          printReply("Hi. I'm a cat. I live in Estonia. Meow.");
         } else if (loadThis === "languages") {
-          $(".input").after("<br>I have knowledge in HTML, CSS, Python, Ruby, JavaScript, PHP, C++ and Java. Please note knowledge does not mean I'm a professional in all of these languages. I probably write a lot of unclean code but oh well ¯\\_(ツ)_/¯");
+          printReply("I have knowledge in HTML, CSS, Python, Ruby, JavaScript, PHP, C++ and Java. Please note knowledge does not mean I'm a professional in all of these languages. I probably write a lot of unclean code but oh well ¯\\_(ツ)_/¯");
         } else {
-          $(".input").after("<br>Cannot load this. Please execute \"load --help\" for help on loading.")
+          printReply("Cannot load this. Please execute \"load --help\" for help on loading.")
         }
       }
 
       else if (cmd.split(" ")[0] === "ls" || cmd.split(" ")[0] === "cd") {
-        $(".input").after("<br>I know, I know. This all is really interesting, but it's not a real terminal. There is no files here.")
+        printReply("I know, I know. This all is really interesting, but it's not a real terminal. There is no files here.")
       }
 
       else if (cmd.split(" ")[0] === "setbg") {
         if (/(^#[0-9A-Fa-f]{6}$)|(^#[0-9A-Fa-f]{3}$)/i.test(cmd.split(" ")[1])) {
           $("body").css("background", cmd.split(" ")[1]);
         } else {
-          $(".input").after("<br>Invalid hex code.");
+          printReply("Invalid hex code.");
         }
       }
 
@@ -89,25 +94,25 @@ $(document).ready(function() {
         if (/(^#[0-9A-Fa-f]{6}$)|(^#[0-9A-Fa-f]{3}$)/i.test(cmd.split(" ")[1])) {
           $("body").css("color", cmd.split(" ")[1]);
         } else {
-          $(".input").after("<br>Invalid hex code.");
+          printReply("Invalid hex code.");
         }
       }
 
       else if (cmd.split(" ")[0] === "theme") {
-        if (cmd.split(" ")[1] !== "--help") {
+        if (cmd.split(" ")[1] !== "--help" || cmd.split(" ")[1] !== "-h") {
           if (cmd.split(" ")[1] in themes) {
             $("body").css("color", themes[cmd.split(" ")[1]]["text"]);
             $("body").css("background", themes[cmd.split(" ")[1]]["bg"]);
           } else {
-            $(".input").after("<br>No such theme exists, please type \"theme --help\" to get a list of themes.");
+            printReply("No such theme exists, please type \"theme --help\" to get a list of themes.");
           }
         } else {
-          $(".input").after("<br>List of available themes:<br>normal&nbsp;&nbsp;The normal terminal look (white on black).<br>white&nbsp;&nbsp;&nbsp;Inverted terminal (black on white).<br>ubuntu&nbsp;&nbsp;Ubuntu terminal style (light grey on purple).<br>matrix&nbsp;&nbsp;Matrix terminal (light green on black).");
+          printReply("List of available themes:<br>normal&nbsp;&nbsp;The normal terminal look (white on black).<br>white&nbsp;&nbsp;&nbsp;Inverted terminal (black on white).<br>ubuntu&nbsp;&nbsp;Ubuntu terminal style (light grey on purple).<br>matrix&nbsp;&nbsp;Matrix terminal (light green on black).");
         }
       }
 
       else {
-        $(".input").after("<br>"+cmd.split(" ")[0]+": command not found");
+        printReply(""+cmd.split(" ")[0]+": command not found");
       }
     }
   }
