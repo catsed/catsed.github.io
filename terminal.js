f18834cee53bae3da62f4f4027d7be276f291693
catsed <catsed@users.noreply.github.com>
2020-05-19 14:30:34 +0300

    Update terminal.js

--- terminal.js
+++ terminal.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-  var index = 0;
-  var commInd = 0;
-  var commands = [];
+  let index = 0;
+  let commInd = 0;
+  let commands = [];
 
-  var themes = {
+  const themes = {
     "normal": {"text": "#FFFFFF", "bg": "#000000"},
     "ubuntu": {"text": "#DADADA", "bg": "#300A24"},
     "white": {"text": "#000000", "bg": "#FFFFFF"},
@@ -15,29 +15,31 @@ $(document).ready(function() {
   }
   
   function parseCmd(cmd) {
-    if (cmd.split(" ")[0] === "sudo") {
+    cmd = cmd.split(" ");
+    
+    if (cmd[0] === "sudo") {
 
-      if (cmd.split(" ")[1] !== undefined) {
-        if (cmd.split(" ")[1] === "make") {
-          if (cmd.split(" ")[2] === "me" && cmd.split(" ")[3] === "a" && cmd.split(" ")[4] === "sandwich") {
+      if (cmd[1] !== undefined) {
+        if (cmd[1] === "make") {
+          if (cmd[2] === "me" && cmd[3] === "a" && cmd[4] === "sandwich") {
             printReply("Okay.");
-          } else if (cmd.split(" ")[2] === "me" && cmd.split(" ")[3] === "a" && cmd.split(" ")[4] === "cake") {
+          } else if (cmd[2] === "me" && cmd[3] === "a" && cmd[4] === "cake") {
             printReply("The cake is still a lie.");
           } else {
             printReply("sudo: make it yourself");
           }
         }
 
-        else if (cmd.split(" ")[1] === "ls" || cmd.split(" ")[1] === "cd") {
+        else if (cmd[1] === "ls" || cmd[1] === "cd") {
           printReply("Listen, sudo is not going to help you here. There are still no files.")
         }
 
-        else if (cmd.split(" ")[1] === "help") {
+        else if (cmd[1] === "help") {
           printReply("List of useful commands:<br>make&nbsp;&nbsp;&nbsp;&nbsp;Makes something, I guess.")
         }
 
         else {
-          printReply("sudo: "+cmd.split(" ")[1]+": command not found");
+          printReply("sudo: "+cmd[1]+": command not found");
         }
       }
 
@@ -46,22 +48,22 @@ $(document).ready(function() {
       }
     } else {
 
-      if (cmd.split(" ")[0] === "make") {
-        if (cmd.split(" ")[1] === "me" && cmd.split(" ")[2] === "a" && cmd.split(" ")[3] === "sandwich") {
+      if (cmd[0] === "make") {
+        if (cmd[1] === "me" && cmd[2] === "a" && cmd[3] === "sandwich") {
           printReply("What? Make it yourself.");
-        } else if (cmd.split(" ")[1] === "me" && cmd.split(" ")[2] === "a" && cmd.split(" ")[3] === "cake") {
+        } else if (cmd[1] === "me" && cmd[2] === "a" && cmd[3] === "cake") {
           printReply("The cake is a lie.");
         } else {
           printReply("make: make it yourself");
         }
       }
 
-      else if (cmd.split(" ")[0] === "help") {
+      else if (cmd[0] === "help") {
         printReply("List of useful commands:<br>load&nbsp;&nbsp;&nbsp;&nbsp;Loads something. Type \"load --help\" for help on loading.<br>make&nbsp;&nbsp;&nbsp;&nbsp;Makes something, I guess.<br>sudo&nbsp;&nbsp;&nbsp;&nbsp;Administrator privileges (for everyone). Type \"sudo help\" for help with sudo commands.<br>setbg&nbsp;&nbsp;&nbsp;Followed by a hex code. Sets the terminal background colour.<br>settext Followed by a hex code. Sets the terminal text colour.<br>theme&nbsp;&nbsp;&nbsp;Followed by a theme name. Sets the terminal colours according to chosen theme. Type \"theme --help\" for a list of themes.");
       }
 
-      else if (cmd.split(" ")[0] === "load") {
-        var loadThis = cmd.split(" ")[1];
+      else if (cmd[0] === "load") {
+        let loadThis = cmd[1];
         if (loadThis === "--help" || loadThis === "-h") {
           printReply("cats&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Loads kittens. :3<br>about&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Loads some stuff about me.<br>languages Loads info about my knowledge on programming languages.");
         } else if (loadThis === "cats") {
@@ -78,31 +80,31 @@ $(document).ready(function() {
         }
       }
 
-      else if (cmd.split(" ")[0] === "ls" || cmd.split(" ")[0] === "cd") {
-        printReply("I know, I know. This all is really interesting, but it's not a real terminal. There is no files here.")
+      else if (cmd[0] === "ls" || cmd[0] === "cd") {
+        printReply("Please. It's not a real terminal. There are no files here.")
       }
 
-      else if (cmd.split(" ")[0] === "setbg") {
-        if (/(^#[0-9A-Fa-f]{6}$)|(^#[0-9A-Fa-f]{3}$)/i.test(cmd.split(" ")[1])) {
-          $("body").css("background", cmd.split(" ")[1]);
+      else if (cmd[0] === "setbg") {
+        if (/(^#[0-9A-Fa-f]{6}$)|(^#[0-9A-Fa-f]{3}$)/i.test(cmd[1])) {
+          $("body").css("background", cmd[1]);
         } else {
           printReply("Invalid hex code.");
         }
       }
 
-      else if (cmd.split(" ")[0] === "settext") {
-        if (/(^#[0-9A-Fa-f]{6}$)|(^#[0-9A-Fa-f]{3}$)/i.test(cmd.split(" ")[1])) {
-          $("body").css("color", cmd.split(" ")[1]);
+      else if (cmd[0] === "settext") {
+        if (/(^#[0-9A-Fa-f]{6}$)|(^#[0-9A-Fa-f]{3}$)/i.test(cmd[1])) {
+          $("body").css("color", cmd[1]);
         } else {
           printReply("Invalid hex code.");
         }
       }
 
-      else if (cmd.split(" ")[0] === "theme") {
-        if (cmd.split(" ")[1] !== "--help" || cmd.split(" ")[1] !== "-h") {
-          if (cmd.split(" ")[1] in themes) {
-            $("body").css("color", themes[cmd.split(" ")[1]]["text"]);
-            $("body").css("background", themes[cmd.split(" ")[1]]["bg"]);
+      else if (cmd[0] === "theme") {
+        if (cmd[1] !== "--help" || cmd[1] !== "-h") {
+          if (cmd[1] in themes) {
+            $("body").css("color", themes[cmd[1]]["text"]);
+            $("body").css("background", themes[cmd[1]]["bg"]);
           } else {
             printReply("No such theme exists, please type \"theme --help\" to get a list of themes.");
           }
@@ -112,7 +114,7 @@ $(document).ready(function() {
       }
 
       else {
-        printReply(""+cmd.split(" ")[0]+": command not found");
+        printReply(""+cmd[0]+": command not found");
       }
     }
   }
@@ -139,7 +141,7 @@ $(document).ready(function() {
     if(e.keyCode == 13) {
       enterPressed();
     } else if (e.keyCode == 38) {
-      var newCommands = commands.slice().reverse();
+      let newCommands = commands.slice().reverse();
       if (newCommands[commInd] != undefined && commInd != -1) {
         $(".input").val(newCommands[commInd]);
         commInd += 1;
@@ -147,7 +149,7 @@ $(document).ready(function() {
         $(".input").val(newCommands[newCommands.length-1]);
       }
     } else if (e.keyCode == 40) {
-      var newCommands = commands.slice().reverse();
+      let newCommands = commands.slice().reverse();
       commInd -= 1;
       if (newCommands[commInd] != undefined && commInd != -1) {
         $(".input").val(newCommands[commInd]);
